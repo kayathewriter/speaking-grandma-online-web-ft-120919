c9310db5a5d9a68cd5af194f9ffedfbef2d31b59
Mikael Kaya <kayaswork@gmail.com>
2019-12-19 00:10:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ def speak_to_grandma(phrase)
    
    # phrase = 'WHAT DID YOU EAT TODAY?'
    # what is expected return "NO, NOT SINCE 1938!"
-   # is what we are saying (phrase) a
+   # is what we are saying (phrase) all uppercased?
    puts "I LOVE YOU TOO PUMPKIN!"
 end
 
