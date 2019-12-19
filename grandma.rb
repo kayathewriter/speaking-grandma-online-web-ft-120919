2481be171f12024f83f981e903e2b5d1d7c8ff62
Mikael Kaya <kayaswork@gmail.com>
2019-12-19 00:07:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   # if you say I LOVE YOU GRANDMA!
   if phrase == "I LOVE YOU GRANDMA!"
-    # responds with "I lo"
+    # responds with 
  if phrase.upcase
    puts "I LOVE YOU TOO PUMPKIN!"
 end
