4d02a98570786ac0a76bcaecf302a21012339704
Mikael Kaya <kayaswork@gmail.com>
2019-12-19 00:09:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,9 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == phrase.upcase
    return "NO, NOT SINCE 1938!"
+   
+   
+   phrase =
    puts "I LOVE YOU TOO PUMPKIN!"
 end
 
